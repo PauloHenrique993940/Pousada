d3f99eb3db300bb5519524c79b02d954fc00866b
Paulo Henrique Ferreira França <paulohenriqueferreirafranca2@gmail.com>
2025-04-03 07:03:58 -0300

    Fazendo as páginas

--- script/reserva.js
+++ script/reserva.js
@@ -9,3 +9,29 @@ document.getElementById("reservaForm").addEventListener("submit", function(event
         erroHospedes.style.display = "none"; // Oculta a mensagem de erro se for válido
     }
 });
+
+let activeOffset = $('nav ul .active').position().left;
+let activeItemWidth = $('nav ul .active').width();
+
+$('document').ready(function(){
+	$('.dot').css('left', activeOffset + activeItemWidth / 2);
+	let bgColor = $('.active a').css("background-color");
+	$('.dot').css('background-color', bgColor);
+});
+
+$('nav').mouseout(function(){
+	$('.dot').css('left', activeOffset + activeItemWidth / 2);
+	let bgColor = $('.active a').css("background-color");
+	$('.dot').css('background-color', bgColor);
+});
+
+$('nav ul li').hover(function(){
+	let navOffset = $(this).position().left;
+	let navItemWidth = $(this).width();
+	
+	$('.dot').css('left', navOffset + navItemWidth / 2);
+	
+	let bgColor = $('a', this).css("background-color");
+	
+	$('.dot').css('background-color', bgColor);
+});
